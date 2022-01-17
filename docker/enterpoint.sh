318ecda698408aa8d39b83b04ecdd4b1a93ce848
陈子烨 <ziye.chen@hand-china.com>
2022-01-17 15:12:38 +0800

    [fix]调整发布脚本

--- docker/enterpoint.sh
+++ docker/enterpoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 find /usr/share/make-invoice/makeInvoice -name '*.html' | xargs sed -i "s BUILD_QR_CODE_BASE_URL $BUILD_QR_CODE_BASE_URL g"
+find /usr/share/nginx/h5_html -name '*.js' | xargs sed -i "s /OS_ENV/ ${OS_ENV:-/} g"
+find /usr/share/nginx/h5_html -name '*.html' | xargs sed -i "s /OS_ENV/ ${OS_ENV:-/} g"
+find /usr/share/nginx/h5_html -name '*.js' | xargs sed -i "s OS_ENV $OS_ENV g"
+find /usr/share/nginx/h5_html -name '*.html' | xargs sed -i "s OS_ENV $OS_ENV g"
 
 exec "$@"
